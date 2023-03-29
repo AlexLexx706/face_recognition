29fd4d08c34d3d7c8addb6a5e7bb82b1cb35976d
alex <akalmykov@topcon.com>
2023-03-30 01:01:07 +0200

    update

--- 02_face_training.py
+++ 02_face_training.py
@@ -18,7 +18,7 @@ def getImagesAndLabels(path):
         PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
         img_numpy = np.array(PIL_img,'uint8')
         print(f'imagePath:{os.path.split(imagePath)}')
-        id = int(os.path.split(imagePath)[-1].split("."))
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
         faces = detector.detectMultiScale(img_numpy)
         for (x,y,w,h) in faces:
             faceSamples.append(img_numpy[y:y+h,x:x+w])
