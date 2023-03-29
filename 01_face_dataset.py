e4b75f73acfa7c1b43b245b8d430592788e8c9c6
alex <akalmykov@topcon.com>
2023-03-30 00:55:56 +0200

    update

--- 01_face_dataset.py
+++ 01_face_dataset.py
@@ -1,26 +1,45 @@
 import cv2
 import os
-# cam = cv2.VideoCapture(0)
+import sys
+DATA_SET = './dataset'
 
-# cam.set(3, 640) # set video width
-# cam.set(4, 480) # set video height
-cam = cv2.VideoCapture('OUTPUT_FILE-2.avi')
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video width
+cam.set(4, 480) # set video height
+
+# cam = cv2.VideoCapture('OUTPUT_FILE-2.avi')
 
 face_detector = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 # For each person, enter one numeric face id
-face_id = input('\n Enter user ID end press <Enter> ==>  ')
+face_id = int(input('\n Enter user ID end press <Enter> ==>  '))
 print("\n Initializing face capture. Look the camera and wait ...")
 # Initialize individual sampling face count
 count = 0
+index = 0
+
+ids = []
+for f_name in os.listdir(DATA_SET):
+    parts = f_name.split(".")
+    if int(parts[1]) == face_id:
+        ids.append(int(parts[2]))
+ids.sort()
+
+if len(ids):
+    index = ids[-1]
+print(f"start index:{index}")
+
 while(True):
     ret, img = cam.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+        index += 1
         count += 1
         # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+        f_name = f"{DATA_SET}/User.{face_id}.{index}.jpg"
+        print(f_name)
+        cv2.imwrite(f_name, gray[y:y+h,x:x+w])
         cv2.imshow('image', img)
     k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
     if k == 27:
